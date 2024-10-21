c400e5cbee559b6de0021345d9dc19979a7461fc
chani999 <psc0729@naver.com>
2024-10-21 14:12:21 +0900

    init

---
+++ src/main/java/com/book4w/book4w/Book4wApplication.java
@@ -0,0 +1,13 @@
+package com.book4w.book4w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Book4wApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Book4wApplication.class, args);
+    }
+
+}
